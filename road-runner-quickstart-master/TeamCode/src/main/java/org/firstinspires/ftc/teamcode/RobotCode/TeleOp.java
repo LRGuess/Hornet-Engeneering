8228508277c313d949ab4be65cfd91aa40b7a75b
Liam <min3n3lind0@gmail.com>
2023-09-16 14:06:50 -0600

    Clean Teleop

---
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/TeleOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.RobotCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="MotorSpin")
+public class TeleOp extends OpMode {
+
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+    @Override
+    public void init() {
+        leftDrive  = hardwareMap.get(DcMotor.class, "motorFL");
+        rightDrive = hardwareMap.get(DcMotor.class, "motorFR");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        leftDrive.setPower(gamepad1.left_stick_y);
+        rightDrive.setPower(gamepad1.right_stick_y);
+    }
+}
