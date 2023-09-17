998e0b8c1b2aabb51995ce13e33a5ad535fdf62f
Aarav <aaravuno8@gmail.com>
2023-09-17 09:49:05 -0600

    added stuff

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Teleop.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Teleop.java
@@ -11,11 +11,14 @@ public class Teleop extends OpMode {
     private DcMotor leftDrive;
     private DcMotor rightDrive;
     private Servo drone;
+    private Servo grabber;
+    private double speed = 0.5;
     @Override
     public void init() {
         leftDrive  = hardwareMap.get(DcMotor.class, "motorL");
         rightDrive = hardwareMap.get(DcMotor.class, "motorR");
         drone = hardwareMap.get(Servo.class, "droneServo");
+        grabber = hardwareMap.get(Servo.class, "grabberServo");
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -23,12 +26,21 @@ public class Teleop extends OpMode {
 
     @Override
     public void loop() {
-        leftDrive.setPower(gamepad1.left_stick_y);
-        rightDrive.setPower(gamepad1.right_stick_y);
+        leftDrive.setPower(gamepad1.left_stick_y * speed);
+        rightDrive.setPower(gamepad1.right_stick_y * speed);
 
         if(gamepad1.left_trigger == 1 && gamepad1.right_trigger == 1 && gamepad1.a){
             drone.setPosition(10.0);
             drone.setPosition(0.0);
+        }
+
+        if(gamepad1.left_trigger == 1){
+            speed = 0.25;
+        }
+        else if(gamepad1.right_trigger == 1){
+            speed = 1;
+        } else {
+            speed = 0.5;
         }
     }
 }
