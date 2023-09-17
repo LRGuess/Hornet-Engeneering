2b0c9e9bfec3508befb29e81ff99df4a250d7adb
Aarav <aaravuno8@gmail.com>
2023-09-16 19:10:51 -0600

    added stuff

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Teleop.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Teleop.java
@@ -3,16 +3,19 @@ package org.firstinspires.ftc.teamcode.RobotCode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="MotorSpin")
 public class Teleop extends OpMode {
 
     private DcMotor leftDrive;
     private DcMotor rightDrive;
+    private Servo drone;
     @Override
     public void init() {
-        leftDrive  = hardwareMap.get(DcMotor.class, "motorFL");
-        rightDrive = hardwareMap.get(DcMotor.class, "motorFR");
+        leftDrive  = hardwareMap.get(DcMotor.class, "motorL");
+        rightDrive = hardwareMap.get(DcMotor.class, "motorR");
+        drone = hardwareMap.get(Servo.class, "droneServo");
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -22,5 +25,10 @@ public class Teleop extends OpMode {
     public void loop() {
         leftDrive.setPower(gamepad1.left_stick_y);
         rightDrive.setPower(gamepad1.right_stick_y);
+
+        if(gamepad1.left_trigger == 1 && gamepad1.right_trigger == 1 && gamepad1.a){
+            drone.setPosition(10.0);
+            drone.setPosition(0.0);
+        }
     }
 }
