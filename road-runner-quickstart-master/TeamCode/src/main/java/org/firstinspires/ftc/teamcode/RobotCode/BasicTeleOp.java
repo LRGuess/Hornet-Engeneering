90d1376b5f47af0e23e338948be845b22d3ddc65
Liam <min3n3lind0@gmail.com>
2023-09-16 18:23:09 -0600

    super basic for testing

---
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/BasicTeleOp.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.RobotCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class BasicTeleOp extends OpMode {
+
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+    @Override
+    public void init() {
+        leftDrive  = hardwareMap.get(DcMotor.class, "motorFL");
+        rightDrive = hardwareMap.get(DcMotor.class, "motorFR");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        leftDrive.setPower(gamepad1.left_stick_y);
+        rightDrive.setPower(gamepad1.right_stick_y);
+    }
+}
