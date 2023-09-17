86949a0e047dc92553d5d95e0ba7bf9e1c12380a
Liam <min3n3lind0@gmail.com>
2023-09-17 10:56:35 -0600

    Grabber Code

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/BasicTeleOp.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/BasicTeleOp.java
@@ -1,16 +1,21 @@
 package org.firstinspires.ftc.teamcode.RobotCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp(name="Basic TeleOp")
 public class BasicTeleOp extends OpMode {
 
     private DcMotor leftDrive;
     private DcMotor rightDrive;
+    private Servo grabberServo;
     @Override
     public void init() {
-        leftDrive  = hardwareMap.get(DcMotor.class, "motorFL");
-        rightDrive = hardwareMap.get(DcMotor.class, "motorFR");
+        leftDrive  = hardwareMap.get(DcMotor.class, "motorL");
+        rightDrive = hardwareMap.get(DcMotor.class, "motorR");
+        grabberServo = hardwareMap.get(Servo.class, "grabberServo");
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -20,5 +25,16 @@ public class BasicTeleOp extends OpMode {
     public void loop() {
         leftDrive.setPower(gamepad1.left_stick_y);
         rightDrive.setPower(gamepad1.right_stick_y);
+
+        GrabberMovement();
+    }
+
+    private void GrabberMovement() {
+        if (gamepad1.y){
+            grabberServo.setPosition(0);
+        }
+        if (gamepad1.a){
+            grabberServo.setPosition(0.1);
+        }
     }
 }
--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Teleop.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Teleop.java
@@ -11,14 +11,14 @@ public class Teleop extends OpMode {
     private DcMotor leftDrive;
     private DcMotor rightDrive;
     private Servo drone;
-    private Servo grabber;
+    private Servo grabberServo;
     private double speed = 0.5;
     @Override
     public void init() {
         leftDrive  = hardwareMap.get(DcMotor.class, "motorL");
         rightDrive = hardwareMap.get(DcMotor.class, "motorR");
         drone = hardwareMap.get(Servo.class, "droneServo");
-        grabber = hardwareMap.get(Servo.class, "grabberServo");
+        grabberServo = hardwareMap.get(Servo.class, "grabberServo");
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -29,18 +29,29 @@ public class Teleop extends OpMode {
         leftDrive.setPower(gamepad1.left_stick_y * speed);
         rightDrive.setPower(gamepad1.right_stick_y * speed);
 
-        if(gamepad1.left_trigger == 1 && gamepad1.right_trigger == 1 && gamepad1.a){
+        if(gamepad1.right_trigger == 1 && gamepad1.b){
             drone.setPosition(10.0);
             drone.setPosition(0.0);
         }
+        CheckSpeed();
+        GrabberMovement();
+    }
 
-        if(gamepad1.left_trigger == 1){
-            speed = 0.25;
-        }
-        else if(gamepad1.right_trigger == 1){
+    private void CheckSpeed() {
+        if(gamepad1.left_bumper == true) speed = 0.25;
+        else if(gamepad1.right_bumper == true){
             speed = 1;
         } else {
             speed = 0.5;
+        }
+    }
+
+    private void GrabberMovement() {
+        if (gamepad1.y){
+            grabberServo.setPosition(0);
+        }
+        if (gamepad1.a){
+            grabberServo.setPosition(0.1);
         }
     }
 }
